838d7bcf7fb4cca96839193214802019639e081e
ren <appymrafli@gmail.com>
2025-12-23 19:28:46 +0700

    feat: Implement DTOs for product requests and responses, and replace ProductRepo with ProductRepository

--- src/main/java/com/ecommerce/repositories/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.repositories;
-
-import com.ecommerce.models.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepo extends CrudRepository<Product, Long> {
-}
